c18af7715ba23a12ec7acd16b58916b112d8c550
enockarthur01 <enock2004arthur@gmail.com>
2023-03-19 23:22:30 -0700

    Alx C programs

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+
+	printf("Size of a Char:",sizeof(char));
+	printf("Size of an Int:",sizeof(int));
+	printf("Sizeof a Double:",sizeof(double));
+	printf("Size of Float:",sizeof(float));
+	printf("Size of long long int:",sizeof(long long int));
+return 0;
+}
+		
