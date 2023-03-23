4d26c6292952c8fd588df5c11211ab9d77ca6d77
enockarthur01 <enock2004arthur@gmail.com>
2023-03-22 23:01:04 -0700

    nested  loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main -print _putchar
+ * Return: 0
+ */
+
+int main(void){
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('a');
+	_putchar('k');
+	_putchar('j');
+	_putchar('b');
+	_putchar('m');
+	_putchar('c');
+	_putchar('d');
+	_putchar('\n');
+	return(0);
+
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+
+
+int main(){
+	void print_alphabet_x10(char alpha){
+		for(int n=0; n <10;n++){
+
+			_putchar(alpha.tolower('A');
+			_putchar('\n');
+
+			}
+
+			}
+
+
+	return 0;
+
+}
