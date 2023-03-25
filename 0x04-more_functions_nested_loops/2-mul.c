6230325514f4cf655aa2e150aeeab0e9b27b88be
enockarthur01 <enock2004arthur@gmail.com>
2023-03-25 01:57:32 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
+ */
+
+
+
+int multiply(int a, int b){
+	
+	int product;
+	product = a*b;
+
+	return product;
+
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+
+int print_num(void){
+
+	for(char x = '0';x <='9';x++){
+
+		putchar(x);
+
+	}
+	_purchar('\n');
+
+}	
+			
